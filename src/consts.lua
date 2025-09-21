4bb249da0b38335844898361bc4d3f10adc9ecd7
eric-j-owen <ejowen+github@proton.me>
2025-09-20 20:45:07 -0400

    adds state consts

--- src/consts.lua
+++ src/consts.lua
@@ -1,7 +1,18 @@
 --controls
-BTN_LEFT  = 0
-BTN_RIGHT = 1
-BTN_UP    = 2
-BTN_DOWN  = 3
-BTN_O     = 4 --z on keyboard
-BTN_X     = 5 --x on keyboard
+BTN = {
+    LEFT  = 0,
+    RIGHT = 1,
+    UP    = 2,
+    DOWN  = 3,
+    O     = 4, --z on keyboard
+    X     = 5  --x on keyboard
+}
+
+
+--game state\
+GAME = {
+    MENU  = 1,
+    MAIN  = 2,
+    WIN   = 3,
+    OVER  = 4,
+}
